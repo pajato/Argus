3f703df3beda63c25691c73dbf945c65288225e3
Paul Michael Reilly <pmr@pajato.com>
2017-10-04 21:12:23 -0400

    Add and test the empty video list content page.

--- app/src/androidTest/java/com/pajato/argus/MainActivityTest.kt
+++ app/src/androidTest/java/com/pajato/argus/MainActivityTest.kt
@@ -1,3 +1,20 @@
+/*
+* Copyright (C) 2017 Pajato Technologies LLC.
+*
+* This file is part of Pajato GameChat.
+
+* GameChat is free software: you can redistribute it and/or modify it under the terms of the GNU
+* General Public License as published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+
+* GameChat is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+* even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* General Public License for more details.
+
+* You should have received a copy of the GNU General Public License along with GameChat.  If not,
+* see http://www.gnu.org/licenses
+*/
+
 package com.pajato.argus
 
 import android.support.test.InstrumentationRegistry.getInstrumentation
@@ -14,7 +31,6 @@ import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import android.view.Gravity
 import android.view.View
-import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import org.hamcrest.Matcher
 import org.junit.Assert.fail
@@ -35,6 +51,8 @@ class MainActivityTest {
 
     /** Ensure that the FAB button click handler code is invoked. */
     @Test fun testFabButton() {
+        // Ensure that the fab button uses a + icon.
+        checkViewVisibility(withId(R.id.fab), VISIBLE)
         onView(withId(R.id.fab)).perform(click())
         // TODO: ensure the search activity is invoked.
     }
@@ -90,7 +108,8 @@ class MainActivityTest {
     @Test fun testInitialState() {
         checkViewVisibility(withId(R.id.nav_view), INVISIBLE)
         checkViewVisibility(withId(R.id.toolbar), VISIBLE)
-        checkViewVisibility(withId(R.id.helloWorldText), VISIBLE)
+        checkViewVisibility(withId(R.id.emptyListText), VISIBLE)
+        checkViewVisibility(withId(R.id.emptyListText), VISIBLE)
         checkViewVisibility(withId(R.id.fab), VISIBLE)
     }
 
