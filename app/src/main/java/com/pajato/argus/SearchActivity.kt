d6b8f304ab1f82d49ef867c27f859e99d665b714
Bryan Scott <bryan@pajato.com>
2017-10-10 16:30:49 -0400

    UI Tweaks to SearchActivity
    # Rationale
    Upon request, the SearchActivity's user interface has been updated.
    
    # New Files
    
    ## res/drawable/ic_check_black_24dp.xml
    * A new check mark for the screen to serve as the "all done" button.
    
    ## res/menu/save_menu.xml
    * A new menu to be inflated when we enter the save screen -- it only has one item, which is the "Save Button" to trigger the save function.
    
    # Changed Files
    
    ## AndroidManifest
    * Updated the launcher icon to use the preferred version without an Android-Studio-enforced background.
    * Ensured that when you enter to the SearchActivity the keyboard is open by default and the toolbar has an appropriate label.
    
    ## SearchActivity.kt
    * Now correctly sets the support action bar to allow the toolbar to function correctly.
    * Handles the options menu creation, namely to color the save button white.
    * Sets up the networks autocomplete text adapter.
    * Focus is given directly to the title EditText as soon as the activity launches.
    
    ## res/layout/activity_search.xml
    * A large overhaul that focused on keeping the two activities looking similar.
    * Replaced the "Networks" spinner with an AutoCompleteTextView that will allow users to type in custom networks without an extra step, and automatically suggest our already-approved networks.
    * Introduced TextInputLayouts for a smoother and prettier input.
    
    ## res/values/strings.xml
    * Added a new string resource for the SearchActivity toolbar.

--- app/src/main/java/com/pajato/argus/SearchActivity.kt
+++ app/src/main/java/com/pajato/argus/SearchActivity.kt
@@ -1,10 +1,16 @@
 package com.pajato.argus
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.graphics.PorterDuff
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
-import android.view.View
+import android.view.Menu
+import android.view.MenuItem
+import android.view.inputmethod.InputMethodManager
+import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_search.*
 
 class SearchActivity : AppCompatActivity() {
@@ -18,15 +24,38 @@ class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        setSupportActionBar(toolbar)
     }
 
-    fun save(view: View) {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu and color the check icon to be the correct color
+        menuInflater.inflate(R.menu.save_menu, menu)
+        val drawable = menu.getItem(0).icon
+        drawable.mutate()
+        val color = ContextCompat.getColor(applicationContext, android.R.color.white)
+        drawable.setColorFilter(color, PorterDuff.Mode.SRC_ATOP)
+        return true
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // Add the auto-fill resources to the networks input.
+        val networks: List<String> = resources.getStringArray(R.array.networks).toList()
+        val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, networks)
+        network.setAdapter(arrayAdapter)
+        // Force the activity to focus on the name input
+        name.requestFocus()
+        val imm: InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(name, InputMethodManager.SHOW_IMPLICIT)
+    }
+
+    fun save(menuItem: MenuItem) {
 
         if (!name.text.isEmpty()) {
             val result = Intent()
             result.putExtra(TYPE_KEY, "video")
             result.putExtra(TITLE_KEY, name.editableText.toString())
-            result.putExtra(NETWORK_KEY, network.selectedItem.toString())
+            result.putExtra(NETWORK_KEY, network.editableText.toString())
             setResult(Activity.RESULT_OK, result)
         } else {
             setResult(Activity.RESULT_CANCELED)
